be74a388ea3bf1280244c354ac814a8d62b659da
Arthur Guiot <arguiot@gmail.com>
2018-09-02 15:20:10 +0800

    Initial Commit

---
+++ Friction/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Friction
+//
+//  Created by Arthur Guiot on 2/9/18.
+//  Copyright © 2018 Arthur Guiot. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
